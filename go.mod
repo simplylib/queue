9d49ff52711c294ad0fff7e73dfa2926e4f3d394
Clayton Townsend II <clayton@claytontii.com>
2022-10-06 19:43:37 -0500

    inital commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/simplylib/queue
+
+go 1.18
---
+++ queue_test.go
@@ -0,0 +1,57 @@
+package queue
+
+import "testing"
+
+func TestQueue(t *testing.T) {
+	q := Queue[int]{}
+
+	q.Add(1)
+	q.Add(2)
+	q.Add(3)
+
+	if q.All()[0] != 1 && q.All()[1] != 2 && q.All()[2] != 3 {
+		t.Fatal("all does not contain expected data")
+	}
+
+	if s := q.Size(); s != 3 {
+		t.Fatalf("size should be 3, have (%v)\n", s)
+	}
+
+	if !q.More() {
+		t.Fatal("no more in queue after adding")
+	}
+
+	if n := q.Next(); n != 1 {
+		t.Fatalf("first in queue is not 1, got (%v)\n", n)
+	}
+
+	if !q.More() {
+		t.Fatal("should be 2 in queue")
+	}
+
+	if n := q.Next(); n != 2 {
+		t.Fatalf("first in queue is not 2, got (%v)\n", n)
+	}
+
+	if !q.More() {
+		t.Fatal("should be 1 in queue")
+	}
+
+	if n := q.Next(); n != 3 {
+		t.Fatalf("first in queue is not 3, got (%v)\n", n)
+	}
+
+	if q.More() {
+		t.Fatal("should be 0 in queue")
+	}
+
+	q.Add(1)
+	q.Add(2)
+	q.Add(3)
+
+	q.Clear()
+
+	if q.More() {
+		t.Fatal("queue has data after being cleared")
+	}
+}
