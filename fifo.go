b61e131e2c6c5f0fe396b59b451366e5369e229a
Clayton Townsend II <clayton@claytontii.com>
2022-10-06 20:18:10 -0500

    simply logic for appending, make specific sizes

--- fifo.go
+++ fifo.go
@@ -1,6 +1,6 @@
 package queue
 
-// FIFO is a naive implementation of a FIFO genric queue
+// FIFO is a naive implementation of a Generic FIFO queue
 type FIFO[T any] struct {
 	data []T
 }
@@ -8,9 +8,7 @@ type FIFO[T any] struct {
 // Next gets the next element in the queue and removes it from the Queue
 func (q *FIFO[T]) Next() T {
 	n := q.data[0]
-	var newData []T
-	newData = append(newData, q.data[1:]...)
-	q.data = newData
+	q.data = append(make([]T, 0, len(q.data[1:])), q.data[1:]...)
 	return n
 }
 
@@ -31,9 +29,7 @@ func (q *FIFO[T]) Size() int {
 
 // All elements in the queue as a copy of the current queue
 func (q *FIFO[T]) All() []T {
-	var all []T
-	all = append(all, q.data...)
-	return all
+	return append(make([]T, 0, len(q.data)), q.data...)
 }
 
 // Clear Queue back to initial state
